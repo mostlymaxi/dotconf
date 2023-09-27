9f0ab171f8182e401c61ac37bf9e519137558eba
Max <max.saparov@gmail.com>
2023-09-27 13:49:04 -0400

    added pkg install scripts

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -4,14 +4,8 @@ sudo pacman -Syu
 curl https://sh.rustup.rs -sSf | sh
 
 paru --version || sudo pacman -S --needed base-devel && cd /opt && git clone https://aur.archlinux.org/paru.git && makepkg -si
-paru -Syu
+paru
 
-paru --repo wofi --limit 5
-paru --repo helix --limit 5
-paru cava --limit 5
-paru fastfetch --limit 5
-paru --repo fish --limit 5
-paru --repo python3 --limit 5
-paru mpvpaper --limit 5
-
+cat packages.txt | xargs -o paru -S
+cat aur-packages.txt | xargs -o paru -Sa
 
