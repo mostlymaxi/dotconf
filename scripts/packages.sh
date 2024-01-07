c86b0587302b239da123aa4062687d39ab5c29c6
Max Saparov <max.saparov@gmail.com>
2024-01-06 23:46:43 -0500

    fixing install script

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/bash
 
-sudo pacman -Syu
-curl https://sh.rustup.rs -sSf | sh
+install_paru() {
+	sudo pacman -S --needed base-devel	
+	sudo git clone https://aur.archlinux.org/paru.git /opt/paru
+	sudo chown -R $USER: /opt/paru
+	(cd /opt/paru && makepkg -si)
+}
 
-paru --version || sudo pacman -S --needed base-devel && cd /opt && git clone https://aur.archlinux.org/paru.git && makepkg -si
+sudo pacman -Syu
+sudo pacman -Syu rustup
+rustup default stable
+
+paru --version || install_paru
 paru
 
 cat packages.txt | xargs -o paru -S
 cat aur-packages.txt | xargs -o paru -Sa
+
+sudo systemctl enable ly.service
+sudo systemctl enable sshd
+
+sudo mkdir -p /usr/share/fonts/codelia
+sudo gpg -o /usr/share/fonts/codelia/Codelia-Regular.otf -d ~/.config/fonts/Codelia/Codelia-Regular.otf.gpg
+
+mkdir -p ~/.ssh
+cat ~/.config/scripts/*.pub >> ~/.ssh/authorized_keys
 
