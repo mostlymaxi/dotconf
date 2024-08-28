e711206a1b5d7c4eb72867562658d0cc3bd9e619
maxi <31300687+mostlymaxi@users.noreply.github.com>
2024-08-28 13:49:39 -0400

    Maxi/remembering config (#2)
    * Lazy -> AstroNvim
    
    * adding obs config
    
    * oh boy gl
    
    * changed: hyprland conf cursors
    
    * changed: foot default font size
    
    * added: fish functions
    
    * updated packages
    
    * added: obs config
    
    * ignoring secrets
    
    * updated: hyprland config
    
    * added: nvim conf
    
    * added: hyfetch
    
    * added: obs-studio-liberty
    
    * fixing greeter

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,9 @@ function fish_greeting
     fastfetch
 end
 
+# for strim
+alias env="sl -lFa"
+
 alias ls="exa -lha"
 alias cat=bat
 alias top=btm
@@ -10,10 +13,15 @@ alias please=sudo
 alias ff=fastfetch
 alias pip=pip3
 alias hx=helix
+# alias nv=nvim
+alias grep=rg
+alias maxi="printf \n\n\n\n\n\n\n\n\n\n && echo mostlymaxi | toilet | lolcat && printf \n\n\n\n\n\n\n\n"
 
 fish_add_path /usr/local/bin
 fish_add_path .local/bin
 fish_add_path .cargo/bin
 
+function mostly
+    printf \n\n\n\n\n\n\n\n\n\n && echo $argv | toilet -W --termwidth | lolcat && printf \n\n\n\n\n\n\n\n
+end
 
-thefuck --alias | source
